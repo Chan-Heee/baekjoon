ccb98cfb41d1347a9d787ccb6fa1d8289b7f5a38
이찬희 <cksgml019@naver.com>
2024-10-17 14:22:03 +0900

    알파벳

---
+++ DFS/1987.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int r, c, ans;
+char board[34][34];
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+map<char, int> visited;
+string s;
+
+void dfs(int y, int x, int sum)
+{
+    visited[board[y][x]] = 1;
+    ans = max(sum, ans);
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = dy[i] + y;
+        int nx = dx[i] + x;
+
+        if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[board[ny][nx]])
+            continue;
+        dfs(ny, nx, sum + 1);
+        visited[board[ny][nx]] = 0;
+    }
+}
+
+int main()
+{
+    cin >> r >> c;
+    for (int i = 0; i < r; i++)
+    {
+        cin >> s;
+        for (int j = 0; j < c; j++)
+            board[i][j] = s[j];
+    }
+    dfs(0, 0, 1);
+    cout << ans;
+}
