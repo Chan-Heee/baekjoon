fe03ca590436f0013ef560d5938fae444fd88bb5
Chan-Heee <cksgml019@naver.com>
2024-11-12 13:29:01 +0900

    문서검색

--- string/1534.cpp
+++ string/1534.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-string s;
-string f;
+string s, f;
 int ans, pos;
 int main()
 {
     getline(cin, s);
     getline(cin, f);
-
     while ((pos = s.find(f, pos)) != string::npos)
     {
         ans++;
