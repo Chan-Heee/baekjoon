d21e2a950a8ac3bd2a83cba6832a9d3ea5572130
Chan-Heee <cksgml019@naver.com>
2024-11-12 11:27:13 +0900

    동전뒤집기

--- BitMasking/1285.cpp
+++ BitMasking/1285.cpp
@@ -6,60 +6,34 @@ string s;
 int ans;
 bool board[24][24];
 
-void dfs(int cur, int cnt, int last, int isRow)
+void dfs(int cur, int cnt, int last)
 {
     if (cur == cnt)
     {
         int t_sum = 0;
-        if (isRow)
+
+        for (int i = 0; i < n; i++)
         {
-            for (int i = 0; i < n; i++)
+            int h_sum = 0;
+            for (int j = 0; j < n; j++)
             {
-                int h_sum = 0;
-                for (int j = 0; j < n; j++)
-                {
-                    if (!board[j][i])
-                        h_sum++;
-                }
-                (h_sum < n - h_sum) ? t_sum += h_sum : t_sum += n - h_sum;
+                if (!board[j][i])
+                    h_sum++;
             }
-            ans = min(ans, t_sum);
+            t_sum += min(h_sum, n - h_sum);
         }
-        else
-        {
-            for (int i = 0; i < n; i++)
-            {
-                int h_sum = 0;
-                for (int j = 0; j < n; j++)
-                {
-                    if (!board[i][j])
-                        h_sum++;
-                }
-                (h_sum < n - h_sum) ? t_sum += h_sum : t_sum += n - h_sum;
-            }
-            ans = min(ans, t_sum);
-        }
+        ans = min(ans, t_sum);
         return;
     }
+
     for (int i = last + 1; i < n; i++)
     {
 
-        if (isRow)
-        {
-            for (int a = 0; a < n; a++)
-                board[i][a] = !board[i][a];
-            dfs(cur + 1, cnt, i, isRow);
-            for (int a = 0; a < n; a++)
-                board[i][a] = !board[i][a];
-        }
-        else
-        {
-            for (int a = 0; a < n; a++)
-                board[a][i] = !board[a][i];
-            dfs(cur + 1, cnt, i, isRow);
-            for (int a = 0; a < n; a++)
-                board[a][i] = !board[a][i];
-        }
+        for (int a = 0; a < n; a++)
+            board[i][a] = !board[i][a];
+        dfs(cur + 1, cnt, i);
+        for (int a = 0; a < n; a++)
+            board[i][a] = !board[i][a];
     }
 }
 
@@ -87,11 +61,7 @@ int main()
 
     for (int i = 0; i <= n; i++)
     {
-        dfs(0, i, -1, 1);
-    }
-    for (int i = 0; i <= n; i++)
-    {
-        dfs(0, i, -1, 0);
+        dfs(0, i, -1);
     }
     cout << ans;
 }
